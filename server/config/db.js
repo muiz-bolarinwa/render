53bd027767c09bc2a3ea558cc09fe663a26addd4
muiz-bolarinwa <muiz.bolarinwa@dcmail.ca>
2024-04-17 23:41:04 -0400

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SessionSecret = exports.HostName = exports.URI = void 0;
-let LOCAL = false;
+let LOCAL = true;
 let HostName, URI;
 if (LOCAL) {
     exports.URI = URI = "mongodb://localhost/contacts";
     exports.HostName = HostName = "localhost";
 }
 else {
-    exports.URI = URI = "mongodb+srv://johnfrance1:p1UEAikTSTi4UHiu@cluster0.t8tpzjb.mongodb.net/contacts?retryWrites=true&w=majority";
+    exports.URI = URI = "";
     exports.HostName = HostName = "MongoDB Atlas";
 }
 exports.SessionSecret = "INFT2202SessionSecret";
--- server/config/db.ts
+++ server/config/db.ts
@@ -1,15 +1,15 @@
-let LOCAL = false;
+let LOCAL = true;
 let HostName, URI;
 if(LOCAL){
     URI = "mongodb://localhost/contacts";
     HostName = "localhost";
 }else{
-    URI = "mongodb+srv://johnfrance1:p1UEAikTSTi4UHiu@cluster0.t8tpzjb.mongodb.net/contacts?retryWrites=true&w=majority"
+    URI = "mongodb+srv://muiz:bolarinwa@cluster0.vhcl1ge.mongodb.net/";
     HostName = "MongoDB Atlas";
 }
 
 export{URI, HostName};
 export const SessionSecret = "INFT2202SessionSecret";
 
-//mongodb+srv://johnfrance1:p1UEAikTSTi4UHiu@cluster0.t8tpzjb.mongodb.net/contacts?retryWrites=true&w=majority
+
 
